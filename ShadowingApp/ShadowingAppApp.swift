da34adf00cfcd8d6421f8c3c5f9efb9057a128e3
BSH <bshyeon1217@gmail.com>
2026-03-20 16:21:06 +0900

    Initial Commit

---
+++ ShadowingApp/ShadowingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShadowingAppApp.swift
+//  ShadowingApp
+//
+//  Created by BAEKSOHYEON on 3/20/26.
+//
+
+import SwiftUI
+
+@main
+struct ShadowingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
